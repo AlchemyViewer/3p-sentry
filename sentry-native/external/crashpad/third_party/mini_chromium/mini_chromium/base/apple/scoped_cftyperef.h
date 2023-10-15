cc62e9d0d136f8fb2ff986bce2641ca9676e54f9
Rye Mutt <rye@alchemyviewer.org>
2023-10-14 23:15:05 -0400

    sentry-native vendor source 0.6.6

--- sentry-native/external/crashpad/third_party/mini_chromium/mini_chromium/base/apple/scoped_cftyperef.h
+++ sentry-native/external/crashpad/third_party/mini_chromium/mini_chromium/base/apple/scoped_cftyperef.h
@@ -2,14 +2,15 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef MINI_CHROMIUM_BASE_MAC_SCOPED_CFTYPEREF_H_
-#define MINI_CHROMIUM_BASE_MAC_SCOPED_CFTYPEREF_H_
+#ifndef MINI_CHROMIUM_BASE_APPLE_SCOPED_CFTYPEREF_H_
+#define MINI_CHROMIUM_BASE_APPLE_SCOPED_CFTYPEREF_H_
 
 #include <CoreFoundation/CoreFoundation.h>
 
-#include "base/mac/scoped_typeref.h"
+#include "base/apple/scoped_typeref.h"
 
 namespace base {
+namespace apple {
 
 namespace internal {
 
@@ -29,6 +30,7 @@ template <typename CFT>
 using ScopedCFTypeRef =
     ScopedTypeRef<CFT, internal::ScopedCFTypeRefTraits<CFT>>;
 
+}  // namespace apple
 }  // namespace base
 
-#endif  // MINI_CHROMIUM_BASE_MAC_SCOPED_CFTYPEREF_H_
+#endif  // MINI_CHROMIUM_BASE_APPLE_SCOPED_CFTYPEREF_H_
